375850ebcd3880d704c9832ba579f67325400442
GreensterRox <adrian@voteworld.co.uk>
2018-04-30 13:58:02 +0100

    Update convert_playlist.php

--- iTunes Utils/convert_playlist.php
+++ iTunes Utils/convert_playlist.php
@@ -1,7 +1,12 @@
 <?php
 /*
->> mv /media/sf_Ubuntu_local/iTunes\ Music\ Library.xml /home/adrian/projects/create_playlist/
->> cp /media/sf_Ubuntu_local/convert_playlist.php . ; php -f convert_playlist.php "165 (ex)"
+>> I keep a copy of entire music collection (mp3s) on the ubuntu shared drive (safer to have it work on a copy).
+>> This will need refreshing every now and then otherwise a playlist might refer to a song (details) that has been altered.
+>> sudo su -
+>> cd /home/adrian/projects/create_playlist/
+>> mv /media/sf_Ubuntu_local/iTunes\ Music\ Library.xml ~/projects/create_playlist/
+>> cp /media/sf_Ubuntu_local/convert_playlist.php . ; php -f convert_playlist.php "100 (ex)"
+>> mv playlists/*ex* /media/sf_Ubuntu_local/created_playlists/
 
 ** This now build a big assoc array of tracks and locations $TRACKS
 e.g.:
@@ -10,13 +15,19 @@ e.g.:
   [28899]=>
   string(106) "/media/sf_Ubuntu_local/Copy_Of_Itunes_Data/Compilations/Hip Young Guitar Slinger [Disc 2]/2-13 Moanin'.mp3"
   
-NEXT Job 
-1.) find playlist id that we are interested in (via command line args) and copy all thos songs into a directory
-2.) Then mount this temp directory onto docker ffmpeg container and run the encoding process to turn them all into MP3
-3.) Then re-order them numerically based on playlist order
-4.) Test with USB stick
+Also does:
+1.) dynamically pull down images and bake them into mp3 when genertaing playlist
+2.) dynamically honour itunes 'stop at' point when creating playlist
+3.) covert volume (ramp up amplitude) - NEEDS TESTING !
 */
 echo "Starting......"."\n";
+
+//getImage(array(4=>'alanis morrisette',5=>'So Called Chaos'),'./');		// test getImage function
+//getBingImage('Pink singer');
+//getGoogleImage('Pink singer');
+//getWikiImage('Pink singer');
+//die('ded');
+
 if(empty($argv[1])){
 	die('Usage: '."\n".'php -f convert_playlist.php "$playlist_name"'."\n".'e.g.'."\n".'php -f convert_playlist.php "Christmas"'."\n\n");
 } else {
@@ -37,7 +48,7 @@ if(!$fp){
 	die('Where\'s the file dude ?');
 }
 
-$allFields = $sql = $TRACKS = $PLAYLIST_DATA = array();
+$allFields = $sql = $TRACKS = $PLAYLIST_DATA = $TRUNCATE_TRACKS = $VOLUME_TRACKS = array();
 $bad_stuff = array('"','\\');
 
 $counter = $dict_counter = 0;
@@ -48,6 +59,14 @@ $in_song = false;
 $in_playlist = false;
 
 $song_count = 0;
+
+// Blow away
+//$blow_away_sql = 'TRUNCATE itunes.itunes_library';
+//$result = mysql_query($blow_away_sql);
+//if(!$result){
+//	die (mysql_error());
+//}
+
 
 while ($line = utf8_encode(fgets($fp))){
 	$counter++;
@@ -146,6 +165,9 @@ while ($line = utf8_encode(fgets($fp))){
 		}
 		if($db_field == 'play_count'){
 			$thisSong['play_count'] = $db_value;
+		}
+		if($db_field == 'stop_time'){
+			$thisSong['stop_time'] = $db_value;
 		}
 	}
 }
@@ -342,18 +364,20 @@ foreach ($playlist_xref as $playlist_persistent_id => $track_id_array){
 					$display_order = '0'.$order;
 				} else {
 					$display_order = $order;
-				}
+				}		
+				
 				$file_part = explode('/',$TRACKS[$track_id]);
 				
 				//$destination preg_replace('/\s+/','_',$file_part[4])= $playlist_directory.'/'.$display_order.'_'.preg_replace('/\s+|^\d+/','_',basename($TRACKS[$track_id]));
-				$artist = preg_replace('/\s+/','_',$file_part[4]);
+				$artist = preg_replace('/\s+|&|\!/','_',$file_part[4]);
 				if(strtolower($artist) == 'compilations'){
-					$artist = preg_replace('/\s+/','_',$file_part[5]);
+					$artist = preg_replace('/\s+|&|\!/','_',$file_part[5]);
 				}
-				$artist = preg_replace('/\'/','',$artist);
+				$artist = preg_replace('/\'|\(|\)/','',$artist);
 				$trackName = preg_replace('/^\d?.?\d+|\'/','',basename($TRACKS[$track_id]));
-				$trackName = preg_replace('/\s+|\(|\)/','_',$trackName);
+				$trackName = preg_replace('/\s+|\(|\)|\!|&/','_',$trackName);
 				$destination = $playlist_directory.'/'.$display_order.'_'.$artist.'_'.$trackName;
+				//$TRACKS[$track_id] = preg_replace('/!/','\\!',$TRACKS[$track_id]);
 				
 				//var_dump($artist);
 				//var_dump($trackName);
@@ -364,13 +388,32 @@ foreach ($playlist_xref as $playlist_persistent_id => $track_id_array){
 					die('Could not copy  to ['.$destination.']');
 				}
 				
-				// check destination file type - if not mp3 convert it !
-				if(strtolower(substr($destination,-3)) !== 'mp3'){
-					$docker_cmd = 'docker run -it -v '.str_replace(basename($destination),'',$destination).':/files sjourdan/ffmpeg -stats -i /files/'.basename($destination).' /files/'.substr(basename($destination),0,-3).'mp3';
-					echo 'Not an MP3 - CONVERTING ! ['.$destination.']'."\n".$docker_cmd."\n";
-					passthru($docker_cmd);
-					unlink($destination);
+				$truncate_time=false;
+				if(isset($TRUNCATE_TRACKS[$track_id])){
+					$truncate_time = ($TRUNCATE_TRACKS[$track_id] / 1000);
 				}
+				
+				$volume_adjust=false;
+				if(isset($VOLUME_TRACKS[$track_id])){
+					switch($VOLUME_TRACKS[$track_id]){
+						case '255':
+							$volume_adjust	= '2';
+							break;
+						default:
+							die('Unknown volume adjust mapping !! ['.$VOLUME_TRACKS[$track_id].']');
+					}
+				}
+				
+				$image_path = getImage($file_part,$playlist_directory);
+				
+				convert($destination,$image_path,$truncate_time,$volume_adjust);
+				
+				$created_track = substr($destination,0,-4).'_.mp3';
+				if(!filesize($created_track)){
+					die('ERROR: Created EMPTY track ['.$created_track.']');
+				}
+				
+				unlink($image_path);
 				
 				$order++;
 			}
@@ -382,6 +425,193 @@ foreach ($playlist_xref as $playlist_persistent_id => $track_id_array){
 
 //echo 'Stored ['.$xref_count.'] playlist_xref data';
 echo 'Finished'."\n\n";
+
+function convert_with_volume($destination,$image_path,$truncate_time,$volume_adjust){
+	
+	$output_file_name = substr(basename($destination),0,-4).'_converted.wav';
+	
+	$truncate_section = $volume_option = '';
+	if($truncate_time){
+		$truncate_section = '-to '.$truncate_time.' ';
+	}
+	
+	if($volume_adjust){
+		$output_file_name = substr(basename($destination),0,-4).'_converted_vol_'.$volume_adjust.'.wav';
+		$volume_option = '-filter:a "volume='.$volume_adjust.'"';
+	}
+	
+	$docker_cmd = 'docker run -it -v '.str_replace(basename($destination),'',$destination).':/files sjourdan/ffmpeg -stats -i /files/'.basename($destination).' '.($image_path ? '-i /files/'.basename($image_path) : '').' -map 0:0 -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (Front)" '.$volume_option.' '.$truncate_section.'/files/'.$output_file_name;
+	
+	echo 'Converting: ['.$destination.']'."\n".$docker_cmd."\n";
+
+	passthru($docker_cmd);
+	
+	echo 'Finished Conversion'."\n";
+	
+	//unlink($destination);
+		
+}
+
+function convert($destination,$image_path,$truncate_time){
+	
+	$truncate_section = '';
+	if($truncate_time){
+		$truncate_section = '-to '.$truncate_time.' ';
+	}
+	
+	$volume_option = '';
+	if($volume_adjust){
+		$volume_option = '-filter:a "volume='.$volume_adjust.'"';
+	}
+	
+	$docker_cmd = 'docker run -it -v '.str_replace(basename($destination),'',$destination).':/files sjourdan/ffmpeg -stats -i /files/'.basename($destination).' -i /files/'.basename($image_path).' -map 0:0 -map 1:0 -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (Front)" '.$volume_option.' '.$truncate_section.'/files/'.substr(basename($destination),0,-4).'_.mp3';
+	
+	echo 'Converting: ['.$destination.']'."\n".$docker_cmd."\n";
+
+	passthru($docker_cmd);
+	
+	echo 'Finished Conversion';
+	
+	unlink($destination);
+		
+}
+
+function really_old_convert($destination,$image_path){
+	// IDEA: Just convert everything to mp3 ??
+	// check destination file type - if not mp3 convert it !
+	if(strtolower(substr($destination,-3)) !== 'mp3'){
+		$docker_cmd = 'docker run -it -v '.str_replace(basename($destination),'',$destination).':/files sjourdan/ffmpeg -stats -i /files/'.basename($destination).' /files/'.substr(basename($destination),0,-3).'mp3';
+		echo 'Not an MP3 - CONVERTING ! ['.$destination.']'."\n".$docker_cmd."\n";
+		die('ded');
+		//passthru($docker_cmd);
+		unlink($destination);
+	}/* else {
+		// add the album art anyway - where to source images from ?? Maybe pull them down from wikipedia automagically ?? :-)
+		// TO DO !
+		
+		$docker_cmd = 'docker run -it -v '.str_replace(basename($destination),'',$destination).':/files sjourdan/ffmpeg -stats -i /files/'.basename($destination).' -i /files/'.basename($image_path).' -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (Front)" /files/'.substr(basename($destination),0,-3).'mp3';
+		echo 'No conversion needed, just ADDING album art ['.$destination.']'."\n".$docker_cmd."\n";
+		passthru($docker_cmd);
+		die('ded');
+		unlink($destination);
+	}*/
+}
+
+function getWikiImage($search_criteria){
+	// Kinda works if article exists , really needs to do two step search on wiki *then* go into article to retrieve image
+	$url='https://en.wikipedia.org/wiki/'.urlencode(str_replace(' ','_',$search_criteria));
+	
+	echo 'Searching WIKIPEDIA'."\n";
+	echo 'GET: '.$url."\n";
+	$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'));
+	$context  = stream_context_create($options);
+	$data=file_get_contents($url,false,$context);
+	
+	if($data){
+		
+		$image_data=explode('<a href="/wiki/File:',$data);
+		
+		if(!empty($image_data[1])){
+			$top_result = $image_data[1];
+			$url_data = explode('</a>',$top_result);
+			$image_tag = $url_data[0];
+			$tag_parts = explode('"',$image_tag);
+			$image_url = 'https:'.$tag_parts[6];
+			
+			$new_image = $playlist_directory.'/'.preg_replace($stuff,'_',$album).'.jpg';
+			file_put_contents($new_image, file_get_contents($image_url,false,$context));
+			
+			return $new_image;
+		}
+	}
+	
+}
+
+function getGoogleImage($search_criteria){
+	$search_query = $search_criteria; //change this
+	$search_query = urlencode( $search_query );
+	$googleRealURL = "https://www.google.com/search?hl=en&biw=1360&bih=652&tbs=isz%3Alt%2Cislt%3Asvga%2Citp%3Aphoto&tbm=isch&sa=1&q=".$search_query."&oq=".$search_query."&gs_l=psy-ab.12...0.0.0.10572.0.0.0.0.0.0.0.0..0.0....0...1..64.psy-ab..0.0.0.wFdNGGlUIRk";
+	echo 'Searching GOOGLE'."\n";
+	echo 'GET: '.$googleRealURL."\n";
+	// Call Google with CURL + User-Agent
+	$ch = curl_init($googleRealURL);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux i686; rv:20.0) Gecko/20121230 Firefox/20.0');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	$google = curl_exec($ch);   
+	$array_imghtml = explode("\"ou\":\"", $google); //the big url is inside JSON snippet "ou":"big url"
+	foreach($array_imghtml as $key => $value){
+	  if ($key > 0) {
+	    $array_imghtml_2 = explode("\",\"", $value);
+	    $array_imgurl[] = $array_imghtml_2[0];
+	  }
+	}
+	//var_dump($array_imgurl); //array contains the urls for the big images
+	return $array_imgurl[0];
+}
+
+function getBingImage($search_criteria){
+	// DOES NOT WORK :-(
+	$url='https://www.bing.com/images/search?q='.urlencode($search_criteria).'&qs=n&form=QBILPG&sp=-1&ghc=1&pq='.urlencode($search_criteria).'&sc=8-11&sk=&cvid=F58ED3940AD44DBC8C8E321DF71996EA';
+	
+	echo 'Searching BING'."\n";
+	echo 'GET: '.$url."\n";
+	$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'));
+	$context  = stream_context_create($options);
+	$data=file_get_contents($url,false,$context);
+	
+	if($data){
+		
+		$image_data=explode('img height',$data);
+		
+		if(!empty($image_data[1])){
+			$top_result = $image_data[1];
+			$url_data = explode('"',$top_result);
+			
+			$image_url = $url_data[29];
+			
+			return $image_url;
+		}
+	}
+	
+	return false;
+}
+
+function getImage($file_part,$playlist_directory){
+
+	$stuff = array('/&/','/"/','/ /',"/'/",'/\+/','/\[/','/\(/','/\]/','/\)/','/\*/','/\./');
+	$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'));
+	$context  = stream_context_create($options);
+	
+	$album = $file_part[5];
+	if($file_part[4] == 'Compilations'){
+		$artist = $file_part[6];
+	} else {
+		$artist = $file_part[4];
+	}
+	
+	$search = $artist.' '.$album;
+	echo 'Image search criteria: '.$search."\n";
+	
+	foreach(array('getGoogleImage','getWikiImage') as $func){
+		$image_url = $func($search);
+		if($image_url){
+			$new_image = $playlist_directory.'/'.preg_replace($stuff,'_',$album).'.jpg';
+			echo 'New image: '.$new_image;
+			$status = file_put_contents($new_image, file_get_contents($image_url,false,$context));
+			if($status && file_exists($new_image) && mime_content_type($new_image) == 'image/jpeg' && filesize($new_image) > 0){
+				return $new_image;
+			}
+		}
+	}
+
+	
+	echo 'Image not found, using placeholder'."\n";
+	copy('default_image.jpg',$playlist_directory.'/default_image.jpg');
+	return($playlist_directory.'/default_image.jpg');
+}
 
 function process_field2($line){
 	global $allFields;
@@ -453,7 +683,8 @@ function process_playlist($meta_sql){
 }
 
 function store_track($sql_arr){
-	global $TRACKS;
+	global $TRACKS, $TRUNCATE_TRACKS;
+
 	foreach($sql_arr as $value){
 		if(strpos($value,'track_id') !== false){
 			list($f,$v) = explode("=",$value);
@@ -463,15 +694,23 @@ function store_track($sql_arr){
 			list($f,$v) = explode("=",$value);
 			$file_path = trim(trim($v),'"');
 		}
+		if(strpos($value,'stop_time') !== false){
+			list($f,$v) = explode("=",$value);
+			$stop_time = trim(trim($v),'"');
+		}
+		if(strpos($value,'volume_adjustment') !== false){
+			list($f,$v) = explode("=",$value);
+			$volume_adjustment = trim(trim($v),'"');
+		}
 	}
 	
-	if($file_path && $track_id){
+	if(isset($file_path) && isset($track_id)){
 		
 		//*real* special cases here!
 		switch($file_path){
 			case 'file://localhost/C:/Users/Adrian/Music/iTunes/iTunes%20Media/Music/Slipknot/Iowa/02%20People%20';
 				$file_path = '/media/sf_Ubuntu_local/Copy_Of_Itunes_Data/Slipknot/Iowa/02 People = Shit.mp3';
-				break;		
+				break;
 		}
 		
 		$TRACKS[$track_id] = str_replace('file://localhost/C:/Users/Adrian/Music/iTunes/iTunes%20Media/Music/','/media/sf_Ubuntu_local/Copy_Of_Itunes_Data/',$file_path);
@@ -517,6 +756,7 @@ function store_track($sql_arr){
 				 as $replaceFrom => $replaceTo){
 			$TRACKS[$track_id]	 	 = str_replace($replaceFrom,$replaceTo,$TRACKS[$track_id]);
 		}*/
+		
 		if(!stat($TRACKS[$track_id])){
 			echo 'Could not stat file - invalid path';
 			var_dump(count($TRACKS));
@@ -524,6 +764,16 @@ function store_track($sql_arr){
 			var_dump($file_parts);
 			die(var_dump($TRACKS[$track_id]));
 		}
+		
+		// does song need truncating?
+		if(isset($stop_time)){
+			$TRUNCATE_TRACKS[$track_id] = $stop_time;
+		}
+		
+		if(isset($volume_adjustment)){
+			$VOLUME_TRACKS[$track_id] = $volume_adjustment;
+		}
+		
 	} else {
 		echo 'Could not extract track/location info';
 		die(var_dump($sql_arr));
